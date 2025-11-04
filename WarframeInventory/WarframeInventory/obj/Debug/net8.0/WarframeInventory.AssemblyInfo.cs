80c1233e88aba065a731be04d8d071041a7dba84
infacundo2 <bamperri2000gmail.com>
2025-11-04 08:58:48 -0300

    Reliquias funcionando ambas dos

--- WarframeInventory/WarframeInventory/obj/Debug/net8.0/WarframeInventory.AssemblyInfo.cs
+++ WarframeInventory/WarframeInventory/obj/Debug/net8.0/WarframeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391d519d6e2b1821742af188b54ca6e4b3cec515")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62adefaed77ecf6e5e531bb1a8229f0698156a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
