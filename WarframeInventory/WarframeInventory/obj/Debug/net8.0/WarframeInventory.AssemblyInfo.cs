c5d2caccb859be3a58a26a582112b65debbbf11a
infacundo2 <bamperri2000gmail.com>
2025-11-04 11:31:50 -0300

    Out 2.0

--- WarframeInventory/WarframeInventory/obj/Debug/net8.0/WarframeInventory.AssemblyInfo.cs
+++ WarframeInventory/WarframeInventory/obj/Debug/net8.0/WarframeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e97bf7a32cdb719904356330229b99fd6d347fd0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca891f8a149ee739a13049b825c7554ad35e095b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WarframeInventory/WarframeInventory/obj/Release/net8.0/WarframeInventory.AssemblyInfo.cs
+++ WarframeInventory/WarframeInventory/obj/Release/net8.0/WarframeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97127fe19fcaa49857763427cec7fa3807d3d6da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca891f8a149ee739a13049b825c7554ad35e095b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
