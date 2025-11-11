12e8fabd6bb9316954605ff8f88af33c26ec7429
infacundo2 <bamperri2000gmail.com>
2025-11-11 12:53:46 -0300

    Dotnet 414---404

--- WarframeInventory/WarframeInventory/obj/Release/net8.0/WarframeInventory.AssemblyInfo.cs
+++ WarframeInventory/WarframeInventory/obj/Release/net8.0/WarframeInventory.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca891f8a149ee739a13049b825c7554ad35e095b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d2caccb859be3a58a26a582112b65debbbf11a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarframeInventory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
